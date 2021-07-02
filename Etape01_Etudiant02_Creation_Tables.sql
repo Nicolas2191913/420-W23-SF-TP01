aa1ca1784531b237e4ef49352b9253fed0e295b1
Tristan Hamel <2192068@csfoy.ca>
2021-07-02 10:28:01 -0400

    creation de branch creationTablesEtudiant2

--- Etape01_Etudiant02_Creation_Tables.sql
+++ Etape01_Etudiant02_Creation_Tables.sql
@@ -1,14 +1,39 @@
 /* 
  Travail pratique 1
- Nom de la première personne
- --> Nom de la deuxième personne
+ Nicolas Lavigne
+ --> Tristan Hamel
  
- Date  JJ-MM-AAAA                                             
+ Date  02-07-2021                                             
 */
--- DROP DATABASE IF EXISTS PosteClientDFC;
--- CREATE DATABASE IF NOT EXISTS PosteClientDFC;
+ DROP DATABASE IF EXISTS PosteClientDFC;
+ CREATE DATABASE IF NOT EXISTS PosteClientDFC;
 
 USE PosteClientDFC;
 -- -----------------------------------------------------
 -- Étape 1 : Création des tables                      --
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS logiciel;
+DROP TABLE IF EXISTS editeur;
+DROP TABLE IF EXISTS poste_has_logiciel;
+DROP TABLE IF EXISTS usager;
+DROP TABLE IF EXISTS usager_has_poste;
+
+CREATE TABLE logiciel (
+	id_logiciel INT,
+    nom_logiciel VARCHAR(45),
+    nb_license INT,
+    
+
+
+
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+CREATE TABLE espece (
+  id smallint(6) unsigned NOT NULL AUTO_INCREMENT,
+  nom_courant varchar(40) NOT NULL,
+  nom_latin varchar(40) NOT NULL,
+  description text,
+  prix decimal(7,2) unsigned DEFAULT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY nom_latin (nom_latin)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
