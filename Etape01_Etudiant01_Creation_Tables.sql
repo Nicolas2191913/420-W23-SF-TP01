7637343bfafae9d9d2aa37ac339f2a4e38b6c338
Nicolas2191913 <2191913@csfoy.ca>
2021-07-02 13:23:51 -0400

    Modification de l'architecture de création de table

--- Etape01_Etudiant01_Creation_Tables.sql
+++ Etape01_Etudiant01_Creation_Tables.sql
@@ -12,14 +12,46 @@ USE PosteClientDFC;
 -- -----------------------------------------------------
 -- Étape 1 : Création des tables                      --
 -- -----------------------------------------------------
+
+SET foreign_key_checks = 0;
+
 DROP TABLE IF EXISTS poste;
-CREATE TABLE IF NOT EXISTS poste;
+CREATE TABLE IF NOT EXISTS poste(
+	id_poste INT UNSIGNED NOT NULL PRIMARY KEY,
+    desc_poste VARCHAR(45),
+    desc_processeur VARCHAR(45),
+    nb_ram_mo VARCHAR(45),
+    tail_disque_giga INT UNSIGNED,
+    id_fabricant INT UNSIGNED,
+    id_local INT UNSIGNED,
+    FOREIGN KEY (id_fabricant)
+		REFERENCES fabricant (id_fabricant),
+	FOREIGN KEY (id_local)
+		REFERENCES `local` (id_local))
+ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `local`;
-CREATE TABLE IF NOT EXISTS `local`;
+CREATE TABLE IF NOT EXISTS `local`(
+	id_local INT UNSIGNED NOT NULL PRIMARY KEY,
+    nom_local VARCHAR(45))
+ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS peripherique;
-CREATE TABLE IF NOT EXISTS peripherique;
+CREATE TABLE IF NOT EXISTS peripherique(
+	id_peripherique INT UNSIGNED NOT NULL PRIMARY KEY,
+    nom_peripherique VARCHAR(45),
+    id_poste INT UNSIGNED,
+    id_fabricant INT UNSIGNED,
+    FOREIGN KEY (id_poste)
+		REFERENCES poste (id_poste),
+	FOREIGN KEY (id_fabricant)
+		REFERENCES fabricant(id_fabricant))
+ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS fabricant;
-CREATE TABLE IF NOT EXISTS fabricant;
+CREATE TABLE IF NOT EXISTS fabricant(
+	id_fabricant INT UNSIGNED NOT NULL PRIMARY KEY,
+    nom_fabricant VARCHAR(45))
+ENGINE=InnoDB;
+
+SET foreign_key_checks = 1;
