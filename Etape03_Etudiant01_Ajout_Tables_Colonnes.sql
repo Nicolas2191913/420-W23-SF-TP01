dc2937c567f5d3c38e4c489ca6f59c81f60c9672
Nicolas2191913 <2191913@csfoy.ca>
2021-07-02 16:00:42 -0400

    Ajout de la table type_peripherique et les ALTER TABLE demandés

--- Etape03_Etudiant01_Ajout_Tables_Colonnes.sql
+++ Etape03_Etudiant01_Ajout_Tables_Colonnes.sql
@@ -1,12 +1,42 @@
 /* 
  Travail pratique 1
- --> Nom de la première personne
- Nom de la deuxième personne
+ --> Nicolas Lavigne
+ Tristan Hamel
  
- Date  JJ-MM-AAAA                                             
+ Date  02-07-2021                                             
 */
 
 USE PosteClientDFC;
 -- -----------------------------------------------------
 -- Étape 3 : Ajout de tables et de colonnes           --
 -- -----------------------------------------------------
+
+DROP TABLE IF EXISTS type_peripherique;
+CREATE TABLE IF NOT EXISTS type_peripherique(
+	id_type_peripherique INT UNSIGNED NOT NULL PRIMARY KEY,
+	nom_type_peripherique VARCHAR(25))
+ENGINE=InnoDB;
+
+INSERT INTO type_peripherique (id_type_peripherique, nom_type_peripherique)
+	VALUES (1, 'Imprimantes'),
+			(2, 'Souris'),
+			(3,	'Clavier'),
+			(4,	'Écran');
+            
+ALTER TABLE peripherique
+	ADD COLUMN id_type_peripherique INT UNSIGNED,
+    ADD CONSTRAINT fk_id_type_peripherique FOREIGN KEY (id_type_peripherique)
+		REFERENCES type_peripherique (id_type_peripherique);
+
+ALTER TABLE `local`
+	ADD COLUMN nb_bureau INT UNSIGNED,
+	ADD COLUMN nb_prise_ethernet INT UNSIGNED,
+	ADD COLUMN imprimante_id_peripherique INT UNSIGNED,
+		MODIFY nom_local VARCHAR(45) NOT NULL,
+		ADD CONSTRAINT fkimprimante FOREIGN KEY (imprimante_id_peripherique)
+        REFERENCES peripherique (id_type_peripherique),
+		ADD CONSTRAINT uk_nom_local UNIQUE (nom_local);
+
+ALTER TABLE fabricant
+	MODIFY nom_fabricant VARCHAR(45) NOT NULL,
+    ADD CONSTRAINT uk_nom_fabricant UNIQUE (nom_fabricant);
