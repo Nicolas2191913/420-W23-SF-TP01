4876440336b77dc7b26137e8c128acc414ce10b8
Tristan Hamel <2192068@csfoy.ca>
2021-07-04 13:45:25 -0400

    Ajout de la partie insertionDonneesEtudiant2 et Ajustement de la partie creationTablesEtudiant2. Il y a un code d'error 3780 avec la creation des tables

--- Etape01_Etudiant02_Creation_Tables.sql
+++ Etape01_Etudiant02_Creation_Tables.sql
@@ -16,15 +16,17 @@ SET foreign_key_checks = 0;
 
 DROP TABLE IF EXISTS logiciel;
 CREATE TABLE logiciel (
-	id_logiciel INT UNSIGNED ,
+	id_logiciel INT UNSIGNED NOT NULL PRIMARY KEY,
     nom_logiciel VARCHAR(45),
     nb_license INT UNSIGNED,
-    id_editeur INT UNSIGNED
+    id_editeur INT UNSIGNED NOT NULL,
+    FOREIGN KEY(id_editeur)
+		REFERENCES editeur (id_editeur)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
 DROP TABLE IF EXISTS editeur;
 CREATE TABLE editeur (
-	id_editeur INT UNSIGNED,
+	id_editeur INT UNSIGNED PRIMARY KEY,
     nom_lediteur VARCHAR(45)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
@@ -36,15 +38,17 @@ CREATE TABLE poste_has_logiciel (
 
 DROP TABLE IF EXISTS usager;
 CREATE TABLE usager (
-	id_usager INT UNSIGNED,
+	id_usager INT UNSIGNED PRIMARY KEY,
     nom_usager VARCHAR(45),
     prenom_usager VARCHAR(45),
-    telephone_usager VARCHAR(15)
-) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+    telephone_usager VARCHAR(15),
+    local_id_local INT,
+    FOREIGN KEY(local_id_local)
+		REFERENCES `local` (id_local)
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;;
 
 DROP TABLE IF EXISTS usager_has_poste;
 CREATE TABLE usager_has_poste (
 	id_usager INT UNSIGNED,
     id_poste INT UNSIGNED
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
-
