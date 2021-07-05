1f760414e8a24e6bb9462bd430f93bf1f5fe4c9a
Tristan Hamel <2192068@csfoy.ca>
2021-07-04 23:05:59 -0400

    Avancement de l'etape 3 etudiant 2, encore quelques petits bugs a corriger

--- Etape03_Etudiant02_Ajout_Tables_Colonnes.sql
+++ Etape03_Etudiant02_Ajout_Tables_Colonnes.sql
@@ -1,12 +1,35 @@
 /* 
  Travail pratique 1
- Nom de la première personne
- --> Nom de la deuxième personne
+ Nicolas Lavigne
+ --> Tristan Hamel
  
- Date  JJ-MM-AAAA                                             
+ Date  04-07-2021                                          
 */
 
 USE PosteClientDFC;
 -- -----------------------------------------------------
 -- Étape 3 : Ajout de tables et de colonnes           --
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS typeusager;
+CREATE TABLE IF NOT EXISTS typeusager(
+	id_typeUsager INT UNSIGNED PRIMARY KEY,
+    nom_statut VARCHAR(45)
+)ENGINE  = InnoDB;
+
+INSERT INTO typeusager (id_typeUsager, nom_statut)
+	VALUES
+    (1, "Professeur"),
+	(2, "Employé"),
+	(3, "Étudiant");
+	
+    
+ALTER TABLE usager
+	ADD COLUMN Id_typeUsager INT UNSIGNED,
+		ADD CONSTRAINT fk_Id_typeUsager FOREIGN KEY (Id_typeUsager)
+		REFERENCES typeusager (id_typeUsager);
+        
+ALTER TABLE logiciel
+	MODIFY nom_logiciel VARCHAR(45) UNIQUE NOT NULL;
+    
+ALTER TABLE editeur 
+	MODIFY nom_editeur VARCHAR(45) UNIQUE NOT NULL;
