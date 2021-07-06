3dac62e416be97cdc93e227c11a0282c2b00b134
Tristan Hamel <2192068@csfoy.ca>
2021-07-06 14:01:28 -0400

    Etape 1 de la partie 4 MAJDonnees pour etudiant 2

--- Etape04_Etudiant02_MAJ_Donnees.sql
+++ Etape04_Etudiant02_MAJ_Donnees.sql
@@ -1,12 +1,27 @@
 /* 
  Travail pratique 1
- Nom de la première personne
- --> Nom de la deuxième personne
+ Nicolas Lavigne
+ --> Tristan Hamel
  
- Date  JJ-MM-AAAA                                             
+ Date  06-07-2021                                            
 */
 
 USE PosteClientDFC;
 -- -----------------------------------------------------
 -- Étape 4 : Mise à jour des données (Clause UPDATE)  --
 -- -----------------------------------------------------
+SET foreign_key_checks = 0;
+
+UPDATE `local`
+SET nom_local = 'p-409', nb_bureau = 27, nb_prise_ethernet = 27, imprimante_id_peripherique = 678
+WHERE id_local = 1;
+
+UPDATE `local`
+SET nom_local = 'p-416', nb_bureau = 5, nb_prise_ethernet = 6, imprimante_id_peripherique = 673
+WHERE id_local = 6;
+
+UPDATE `local`
+SET nom_local = 'p-418', nb_bureau = 27, nb_prise_ethernet = 27, imprimante_id_peripherique = 679
+WHERE id_local = 2;
+
+SET foreign_key_checks = 1;
