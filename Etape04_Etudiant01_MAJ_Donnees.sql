991b82107a0d3fad743b8b02c562c54bf111b97d
Nicolas2191913 <2191913@csfoy.ca>
2021-07-06 13:44:03 -0400

    Début de la procedure

--- Etape04_Etudiant01_MAJ_Donnees.sql
+++ Etape04_Etudiant01_MAJ_Donnees.sql
@@ -1,12 +1,36 @@
 /* 
  Travail pratique 1
- --> Nom de la première personne
- Nom de la deuxième personne
+ --> Nicolas Lavigne
+ Tristan Hamel
  
- Date  JJ-MM-AAAA                                             
+ Date  06-07-2021                                             
 */
 
 USE PosteClientDFC;
 -- -----------------------------------------------------
 -- Étape 4 : Mise à jour des données (Clause UPDATE)  --
 -- -----------------------------------------------------
+
+SET foreign_key_checks = 0;
+
+SELECT * FROM usager;
+
+UPDATE `local`
+SET nb_bureau = 16,
+	nb_prise_ethernet = 16,
+    imprimante_id_peripherique = 674
+WHERE nom_local = 'P-210';
+
+UPDATE `local`
+SET nb_bureau = 27,
+	nb_prise_ethernet = 27,
+    imprimante_id_peripherique = 676
+WHERE nom_local = 'P-308';
+
+UPDATE `local`
+SET nb_bureau = 27,
+	nb_prise_ethernet = 27,
+    imprimante_id_peripherique = 677
+WHERE nom_local = 'P-309';
+
+SET foreign_key_checks = 1;
